fd221da2d413c2b2874a980b136561e13e961416
gschievelbein <gschievelbein@inf.ufrgs.br>
2023-05-29 15:27:00 +0200

    script

---
+++ swapper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import i3ipc 
+
+i3 = i3ipc.Connection()
+focused = i3.get_tree().find_focused()
+windows = i3.get_tree().find_focused().workspace().leaves()
+
+if (len(windows)>1):
+  largest_size=0
+  for w in windows:
+    w_size = w.rect.height*w.rect.width
+    if (w.focused is False and w_size>largest_size):
+      largest = w
+      largest_size=w_size
+  i3.command("mark focused")
+  largest.command("focus")
+  i3.command("swap container with mark focused")
+  i3.command("unmark focused")
